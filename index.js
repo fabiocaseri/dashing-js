57bc7eeb4eb964f439c80d75ea69ad4fe0d349fb
Fabio Caseri <fabio.caseri@gmail.com>
2013-05-03 15:36:22 +0200

    Added exports for some utility modules

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-
 module.exports = require('./lib/dashing');
 
+// Exports utility modules
+module.exports['async'] = require('async');
+module.exports['fs-extra'] = require('fs-extra');
+module.exports['request'] = require('request');
+module.exports['walker'] = require('walker');
