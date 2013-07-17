7fab9846939be6d2bc1160b14ba12182e151ab4e
Fabio Caseri <fabio.caseri@gmail.com>
2013-07-17 09:25:30 +0200

    Updated mincer tests

--- test/mincer_test.js
+++ test/mincer_test.js
@@ -4,51 +4,47 @@ var path = require('path')
 
 describe('Mincer', function() {
   var templateProjectPath = path.resolve(__dirname, '../templates/project');
-
   var mincer = {};
-  mincer.environment = new Mincer.Environment();
-  mincer.assets_prefix = '/assets';
-  mincer.environment.appendPath(templateProjectPath + '/assets/javascripts');
-  mincer.environment.appendPath(templateProjectPath + '/assets/stylesheets');
-  mincer.environment.appendPath(templateProjectPath + '/assets/fonts');
-  mincer.environment.appendPath(templateProjectPath + '/assets/images');
-  mincer.environment.appendPath(templateProjectPath + '/widgets');
-  mincer.environment.appendPath(path.resolve(__dirname, '../javascripts'));
 
-  describe('application.js', function() {
-    it('should compile without errors', function(done) {
-      mincer.environment.findAsset('application.js').compile(function(err, asset) {
-        done();
-      });
+  before(function(done) {
+    mincer.environment = new Mincer.Environment();
+    mincer.assets_prefix = '/assets';
+    mincer.environment.appendPath(templateProjectPath + '/assets/javascripts');
+    mincer.environment.appendPath(templateProjectPath + '/assets/stylesheets');
+    mincer.environment.appendPath(templateProjectPath + '/assets/fonts');
+    mincer.environment.appendPath(templateProjectPath + '/assets/images');
+    mincer.environment.appendPath(templateProjectPath + '/widgets');
+    mincer.environment.appendPath(path.resolve(__dirname, '../javascripts'));
+
+    done();
+  });
+
+  it('should compile "application.js" without errors', function(done) {
+    mincer.environment.findAsset('application.js').compile(function(err, asset) {
+      done();
     });
   });
   
 
-  describe('application.css', function() {
-    it('should compile without errors', function(done) {
-      mincer.environment.findAsset('application.css').compile(function(err, asset) {
-        done();
-      });
+  it('should compile "application.css" without errors', function(done) {
+    mincer.environment.findAsset('application.css').compile(function(err, asset) {
+      done();
     });
   });
 
   /* Mincer 0.5.x */
   /*
-  describe('application.js', function() {
-    it('should compile without errors', function(done) {
-      if (mincer.environment.findAsset('application.js')) {
-        done();
-      }
-    });
+  it('should compile "application.js" without errors', function(done) {
+    if (mincer.environment.findAsset('application.js')) {
+      done();
+    }
   });
   
 
-  describe('application.css', function() {
-    it('should compile without errors', function(done) {
-      if (mincer.environment.findAsset('application.css')) {
-        done();
-      }
-    });
+  it('should compile "application.css" without errors', function(done) {
+    if (mincer.environment.findAsset('application.css')) {
+      done();
+    }
   });
   */
 
